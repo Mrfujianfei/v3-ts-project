8d84bbd71c27a37a45fe40020d7c6f7a90acb24e
lukasi <>
2021-07-15 11:55:20 +0800

    测试husky

---
+++ src/views/home/home.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>homePage</div>
+  <h1>{{ name }}</h1>
+  <el-button type="danger">危险按钮</el-button>
+  <button
+    @click="setName"
+    @drag="setName"
+    @mousedown="setName"
+    @mouseover="setName"
+  >
+    修改名称
+  </button>
+</template>
+<script lang="ts">
+import { defineComponent } from 'vue';
+import { useStore } from 'vuex';
+import { key } from '@/store';
+
+export default defineComponent({
+  name: 'homePage',
+  setup: () => {
+    const store = useStore(key);
+    return {
+      name: store.state.user.name,
+      setName: () => store.commit('user/setName', '小张'),
+    };
+  },
+});
+</script>
