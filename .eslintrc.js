deb141177bd493f20e654e3cf917cc3a858c58c5
lukasi <>
2021-07-18 14:49:59 +0800

    build: 开发架构

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,10 +14,11 @@ module.exports = {
   rules: {
     'vue/no-multiple-template-root': 'off', // 解决template中最顶层只能返回一个元素的检测报错
     'import/extensions': [
+      // 其他文件可不加扩展，引入vue文件需要加
       'error',
       'never',
       {
-        vue: 'always',
+        vue: 'always', // 在ts中引入vue模块不加.vue后缀ts会找不到模块，这边验证vue文件的引用一定加.vue后缀
       },
     ],
     'import/no-unresolved': [
